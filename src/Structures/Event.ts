d05f0fbf6ec82730d0cc720248b3dfa15d2d47b2
Derpinou <lightcompte@gmail.com>
2022-02-28 14:58:54 +0100

    ⚡️ Optimisation

--- src/Structures/Event.ts
+++ src/Structures/Event.ts
@@ -2,9 +2,12 @@ import { Bot } from "./Client";
 
 
 export abstract class BaseEvent {
+    
     protected client: Bot;
+
     constructor(client: Bot) {
         this.client = client;
     }
+
     abstract run(...args: any[]) : void
 }
