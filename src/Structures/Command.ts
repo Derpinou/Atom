d40cbaac0e14834d6226e8f9b5de9e353ed287ab
Derpinou <lightcompte@gmail.com>
2022-02-28 15:01:48 +0100

    ✏️Fix typos

--- src/Structures/Command.ts
+++ src/Structures/Command.ts
@@ -1,30 +1,32 @@
 import { Bot } from "./Client";
 import { CommandInteraction } from "discord.js";
 import {sep} from 'path'
+import type { RESTPostAPIApplicationCommandsJSONBody } from 'discord-api-types/v9'; 
 
 interface CommandOptions {
     filename: string
     enabled?: boolean
     adminOnly?: boolean
+    bodyForm:  RESTPostAPIApplicationCommandsJSONBody
 }
-
-
 
 export abstract class BaseCommand {
     protected client: Bot;
     public enabled: boolean;
     public name: string;
     public adminOnly: boolean;
+    public bodyForm: RESTPostAPIApplicationCommandsJSONBody;
     constructor(client: Bot, {
         filename = "Unknow",
         enabled = true,
-
+        bodyForm,
         adminOnly = false
     }: CommandOptions) {
         this.client = client;
         this.name = filename ? filename.split(sep)[filename.split(sep).length - 1].replace('.js', "").toLowerCase(): 'Unkown';
         this.enabled = enabled;
         this.adminOnly = adminOnly;
+        this.bodyForm = bodyForm;
     }
     abstract run(interaction: CommandInteraction, data: any) : void
 }
