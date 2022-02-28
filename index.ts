4d7425dbc6be385a8d495c079b557a71f1259a3b
Derpinou <lightcompte@gmail.com>
2022-02-28 15:02:43 +0100

    💄Add some spaces

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
-import {Bot} from './src/Structures/Client';
-import {config} from "dotenv";
+import { Bot } from './src/Structures/Client';
+import { config } from "dotenv";
 config();
+
 new Bot(process.env.TOKEN as string);
